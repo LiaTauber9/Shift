fdae6e31e3a62ed3d5bd4b0d2c691668a382ad08
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2023-06-03 12:02:40 +0300

    root package.json

--- server/server.js
+++ server/server.js
@@ -6,31 +6,31 @@ import db from './config/elephantsql.js';
 import users_router from './routes/Users.js';
 import week_router from './routes/Week.js';
 import timesheet_router from './routes/Timesheet.js';
-// import path from 'path';
+import path from 'path';
 
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 dotenv.config();
 const app = express();
-// const buildPath = path.join(__dirname,'..','client','build');
+const buildPath = path.join(__dirname,'..','client','build');
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.use(express.static(buildPath));
+app.use(express.static(buildPath));
 app.use(users_router);
 app.use(week_router);
 app.use(timesheet_router)
 
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.join(buildPath,'index.html'))
-// })
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(buildPath,'index.html'))
+})
 
 app.listen(process.env.PORT||8080, ()=>{
   console.log(`run on ${process.env.PORT||8080}`);
